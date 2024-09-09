3b9a848cca327e8121a6206184c5ba12764abfe4
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-09 14:33:38 +0100

    Update render.c

--- source/render.c
+++ source/render.c
@@ -24,12 +24,14 @@ void render(SDL_Renderer *renderer, double playerX, double playerY, double playe
     int x;
     double rayAngle, distanceToWall, stepSize = 0.1;
     int lineHeight, drawStart, drawEnd, hit, testX, testY;
+    int isVerticalHit;  /* Track if the wall hit is vertical (N/S) or horizontal (E/W) */
 
     for (x = 0; x < SCREEN_WIDTH; x++)
     {
         rayAngle = (playerAngle - FOV / 2.0) + (x / (double)SCREEN_WIDTH) * FOV;
         distanceToWall = 0;
         hit = 0;
+        isVerticalHit = 0;
 
         while (!hit && distanceToWall < 30)
         {
@@ -45,6 +47,10 @@ void render(SDL_Renderer *renderer, double playerX, double playerY, double playe
             else if (map[testY][testX] == 1)
             {
                 hit = 1;
+
+                /* Determine if the hit was vertical (N/S) or horizontal (E/W) */
+                if (fabs(cos(rayAngle)) > fabs(sin(rayAngle)))
+                    isVerticalHit = 1;  /* More horizontal movement -> Vertical wall (N/S) */
             }
         }
 
@@ -55,7 +61,12 @@ void render(SDL_Renderer *renderer, double playerX, double playerY, double playe
         SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255); /* Sky color */
         SDL_RenderDrawLine(renderer, x, 0, x, drawStart);
 
-        SDL_SetRenderDrawColor(renderer, 192, 192, 192, 255); /* Wall color */
+        /* Set wall color depending on its orientation */
+        if (isVerticalHit)
+            SDL_SetRenderDrawColor(renderer, 192, 192, 192, 255); /* Wall facing N/S */
+        else
+            SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255); /* Wall facing E/W */
+
         SDL_RenderDrawLine(renderer, x, drawStart, x, drawEnd);
 
         SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255); /* Floor color */
